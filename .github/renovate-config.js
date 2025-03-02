a6a7434527ff7fc3a3df5c7961296c85f15daf31
Benjamin VIELLARD <gbicou@gmail.com>
2025-03-02 12:39:43 +0100

    ci: Various workflows

---
+++ .github/renovate-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+  platform: 'github',
+  onboarding: false,
+  requireConfig: 'optional',
+  dependencyDashboard: true,
+  repositories: ['gbicou/directus-extension-pdf-image'],
+  addLabels: ['dependencies'],
+  extends: ['config:recommended'],
+  hostRules: [],
+  packageRules: [
+    {
+      groupName: 'Directus',
+      matchPackageNames: [
+        'directus',
+        '@directus/**',
+      ],
+    },
+  ],
+  lockFileMaintenance: {
+    enabled: true,
+    automerge: true,
+  },
+}
