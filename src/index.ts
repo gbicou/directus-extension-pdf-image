d2020ffc013fa838624f4169bbc3f693217f47db
Benjamin VIELLARD <benjamin.viellard@acocia-agaps.com>
2025-08-27 14:07:55 +0200

    fix: upgrade to newer directus api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { defineHook } from '@directus/extensions-sdk'
-import { AssetsService } from '@directus/api/services/assets'
-import { FilesService } from '@directus/api/services/files'
 import { pdf } from 'pdf-to-img'
 import { Readable } from 'node:stream'
 
-export default defineHook(({ action }, { services }) => {
+export default defineHook(({ action }, { services, getSchema }) => {
   // hook on each PDF file upload
   action('files.upload', async ({ payload, key }, context) => {
     if (payload.type === 'application/pdf') {
-      const assets: AssetsService = new services.AssetsService(context)
+      const schema = await getSchema()
+      const assets = new services.AssetsService({
+        schema,
+        accountability: context.accountability,
+      })
       const { stream } = await assets.getAsset(key)
 
       // convert pdf first page to png
@@ -22,7 +24,10 @@ export default defineHook(({ action }, { services }) => {
         payload.filename_disk = payload.filename_disk.replace(/\.pdf$/i, '') + '.png'
 
         // upload image to directus files
-        const files: FilesService = new services.FilesService(context)
+        const files = new services.FilesService({
+          schema,
+          accountability: context.accountability,
+        })
         await files.uploadOne(Readable.from(image), {
           ...payload,
         })
