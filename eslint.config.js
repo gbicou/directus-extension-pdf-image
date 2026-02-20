f8e6a7fa9bbefcca49f653f1df24904740eebef7
Benjamin VIELLARD <gbicou@gmail.com>
2026-02-20 20:50:36 +0100

    refactor: use defineConfig in eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,18 @@
+import { defineConfig } from 'eslint/config'
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 
-export default ts.config(
+export default defineConfig(
   {
     ignores: ['dist/'],
   },
   // javascript
   js.configs.recommended,
   // typescript
-  ...ts.configs.recommended,
-  ...ts.configs.strict,
-  ...ts.configs.stylistic,
+  ts.configs.recommended,
+  ts.configs.strict,
+  ts.configs.stylistic,
   // stylistic
   stylistic.configs.recommended,
 )
